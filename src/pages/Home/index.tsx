943165ca074f4351434795d0b505d715b3209602
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-04 23:45:34 -0300

    wip: home page wip

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,18 @@
 import { Play } from "phosphor-react";
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
 import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa.'),
+  minutesAmount: zod.number().min(5, 'O ciclo precisa ser de no minuto 5 minutos.').max(60, 'O ciclo precisa ser de no máximo 60 minutos.')
+})
 
 export function Home() {
-  const {register, handleSubmit, watch } = useForm()
+  const {register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema)
+  })
 
   function handleCreateNewCycle(data: any) {
     console.log(data)
