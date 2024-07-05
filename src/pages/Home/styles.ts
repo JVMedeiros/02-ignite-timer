b6b478edae63456f3cabdad5f7d8aa9d67cf83e7
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-05 12:12:23 -0300

    feat: add stop countdownbutton styles

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -55,7 +55,7 @@ export const Separator = styled.div`
   justify-content: center;
 `;
 
-export const StartCountdownButton = styled.button`
+export const BaseCountdownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
@@ -70,16 +70,11 @@ export const StartCountdownButton = styled.button`
 
   cursor: pointer;
 
-  background: ${(props) => props.theme['green-500']};
   color: ${(props) => props.theme['gray-100']};
 
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
-  }
-
-  &:not(:disabled):hover {
-    background: ${(props) => props.theme['green-700']};
   }
 `;
 
@@ -113,4 +108,20 @@ export const TaskInput = styled(BaseInput)`
 
 export const MinutesAmountInput = styled(BaseInput)`
   width: 4rem;
+`;
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['green-500']};
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['green-700']};
+  }
+`;
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['red-500']};
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['red-700']};
+  }
 `;
