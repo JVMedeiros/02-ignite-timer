f7cf138faa2ad854a4a6b8e6601f7f4a3e8b1926
João Medeiros <jv.medeiros.gallina@gmail.com>
2024-07-05 12:58:38 -0300

    wip: newCycleForm component wip

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,40 @@
+import { FormContainer, MinutesAmountInput, TaskInput } from "./styles";
+
+export function NewCycleForm() {
+  return (
+    <FormContainer>
+    <label htmlFor="task">Vou trabalhar em</label>
+    <TaskInput 
+      id="task" 
+      list="task-sugestions" 
+      placeholder="Dê um nome para o seu projeto"
+      disabled={!!activeCycle}
+      {...register('task')}
+    />
+
+    <datalist id="task-sugestions">
+      <option value="HStory Front"></option>
+      <option value="HStory Back"></option>
+      <option value="Ignite React"></option>
+      <option value="Ignite Node"></option>
+      <option value="Ignite IA"></option>
+      <option value="Ignite"></option>
+    </datalist>
+
+    <label htmlFor="minutesAmount">durante</label>
+    <MinutesAmountInput 
+      type="number" 
+      id="minutesAmount" 
+      placeholder="00" 
+      step={5} 
+      min={5} 
+      max={60}
+      disabled={!!activeCycle}
+      {...register('minutesAmount', { valueAsNumber: true })}
+
+    />
+
+    <span>minutos.</span>
+  </FormContainer>
+  )
+}
